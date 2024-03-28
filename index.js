0eac2b830096c9b8f2b51e9fb13b7b2661c83bc9
lisapeters13 <lisapetersirl@gmail.com>
2024-03-28 18:42:59 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+   cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+cats.pop(name);
+}
+function  destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function appendCat(name) {
+    var newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+}
+function prependCat(name) {
+var newArray =cats.slice();
+newArray.unshift(name)
+return newArray
+}
+function removeLastCat(name) {
+    var newArray = cats.slice();
+    newArray.pop(name)
+    return newArray
+}
+function removeFirstCat(name) {
+    var newArray = cats.slice();
+    newArray.shift(name)
+    return newArray
+}
